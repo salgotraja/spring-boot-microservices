f4a1e940acec9e679c6cbd89c10417f4d43451d6
Jagdish Salgotra <jagdishs@ndtv.com>
2024-04-17 20:37:12 +0530

    Order service

---
+++ order-service/src/main/java/com/js/bookstore/orders/ApplicationProperties.java
@@ -0,0 +1,12 @@
+package com.js.bookstore.orders;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "orders")
+public record ApplicationProperties(
+        String orderEventsExchange,
+        String newOrdersQueue,
+        String deliveredOrdersQueue,
+        String cancelledOrdersQueue,
+        String errorOrdersQueue
+) { }
---
+++ order-service/src/main/java/com/js/bookstore/orders/domain/OrderNotFoundException.java
@@ -0,0 +1,11 @@
+package com.js.bookstore.orders.domain;
+
+public class OrderNotFoundException extends RuntimeException{
+    public OrderNotFoundException(String message) {
+        super(message);
+    }
+
+    public static OrderNotFoundException forOrderNumber(String orderNumber) {
+        return new OrderNotFoundException("Order with number: " + orderNumber);
+    }
+}
