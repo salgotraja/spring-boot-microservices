663172833001432e0af60e811ed28bd12f0523f9
Jagdish Salgotra <jagdishsal@gmail.com>
2024-04-27 19:56:23 +0530

    order service

---
+++ order-service/src/main/java/com/js/bookstore/orders/domain/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.js.bookstore.orders.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+interface OrderRepository extends JpaRepository<OrderEntity, Long> {
+
+}
---
+++ order-service/src/main/java/com/js/bookstore/orders/domain/OrderValidator.java
@@ -0,0 +1,37 @@
+package com.js.bookstore.orders.domain;
+
+import com.js.bookstore.orders.clients.catalog.Product;
+import com.js.bookstore.orders.clients.catalog.ProductServiceClient;
+import com.js.bookstore.orders.domain.models.CreateOrderRequest;
+import com.js.bookstore.orders.domain.models.OrderItem;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.util.Set;
+
+@Component
+class OrderValidator {
+    private static final Logger log = LoggerFactory.getLogger(OrderValidator.class);
+
+    private final ProductServiceClient client;
+
+    OrderValidator(ProductServiceClient client) {
+        this.client = client;
+    }
+
+    void validate(CreateOrderRequest request) {
+        Set<OrderItem> items = request.items();
+        for(OrderItem item : items) {
+            Product product = client.getProductByCode(item.code())
+                    .orElseThrow(() -> new InvalidOrderException("Invalid Product code: " + item.code()));
+            if(item.price().compareTo(product.price()) != 0) {
+                log.error(
+                        "Product price not matching. Actual price:{}, received price:{}",
+                        product.price(),
+                        item.price());
+                throw new InvalidOrderException("Product price not matching");
+            }
+        }
+    }
+}
