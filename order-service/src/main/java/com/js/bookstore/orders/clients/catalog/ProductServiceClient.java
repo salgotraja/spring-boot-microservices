9436e960fb0faa38ce903c540d76bb2a68336bbd
Jagdish Salgotra <jagdishsal@gmail.com>
2024-05-06 17:46:52 +0530

    code cleanup

--- order-service/src/main/java/com/js/bookstore/orders/clients/catalog/ProductServiceClient.java
+++ order-service/src/main/java/com/js/bookstore/orders/clients/catalog/ProductServiceClient.java
@@ -23,11 +23,8 @@ public class ProductServiceClient {
     public Optional<Product> getProductByCode(String code) {
         log.info("Fetching product for code: {}", code);
 
-        var product = restClient
-                .get()
-                .uri("/api/products/{code}", code)
-                .retrieve()
-                .body(Product.class);
+        var product =
+                restClient.get().uri("/api/products/{code}", code).retrieve().body(Product.class);
 
         return Optional.ofNullable(product);
     }
--- order-service/src/main/java/com/js/bookstore/orders/domain/OrderService.java
+++ order-service/src/main/java/com/js/bookstore/orders/domain/OrderService.java
@@ -2,22 +2,21 @@ package com.js.bookstore.orders.domain;
 
 import com.js.bookstore.orders.domain.models.CreateOrderRequest;
 import com.js.bookstore.orders.domain.models.CreateOrderResponse;
+import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 @Service
 @Transactional
 public class OrderService {
 
     private static final Logger log = LoggerFactory.getLogger(OrderService.class);
-     private static final List<String> DELIVERY_ALLOWED_COUNTRIES = List.of("INDIA", "USA", "GERMANY", "UK");
+    private static final List<String> DELIVERY_ALLOWED_COUNTRIES = List.of("INDIA", "USA", "GERMANY", "UK");
 
     private final OrderRepository orderRepository;
-     private final OrderValidator orderValidator;
+    private final OrderValidator orderValidator;
     private final OrderEventService orderEventService;
 
     OrderService(OrderRepository orderRepository, OrderValidator orderValidator, OrderEventService orderEventService) {
@@ -27,7 +26,7 @@ public class OrderService {
     }
 
     public CreateOrderResponse createOrder(String userName, CreateOrderRequest request) {
-         orderValidator.validate(request);
+        orderValidator.validate(request);
         OrderEntity newOrder = OrderMapper.convertToEntity(request);
         newOrder.setUserName(userName);
         OrderEntity savedOrder = this.orderRepository.save(newOrder);
