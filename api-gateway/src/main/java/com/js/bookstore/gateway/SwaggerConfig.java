ff863de532d603900f667acef54b17165f92e9d3
Jagdish Salgotra <jagdishsal@gmail.com>
2024-10-11 00:25:03 +0530

    swagger documentation
    Took 30 minutes

--- api-gateway/src/main/java/com/js/bookstore/gateway/SwaggerConfig.java
+++ api-gateway/src/main/java/com/js/bookstore/gateway/SwaggerConfig.java
@@ -5,7 +5,6 @@ import static org.springdoc.core.utils.Constants.DEFAULT_API_DOCS_URL;
 import jakarta.annotation.PostConstruct;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Objects;
 import java.util.Set;
 import org.springdoc.core.properties.AbstractSwaggerUiConfigProperties;
 import org.springdoc.core.properties.SwaggerUiConfigProperties;
@@ -14,24 +13,21 @@ import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class SwaggerConfig {
-
-    private final RouteDefinitionLocator routeDefinitionLocator;
+class SwaggerConfig {
+    private final RouteDefinitionLocator locator;
     private final SwaggerUiConfigProperties swaggerUiConfigProperties;
 
-    public SwaggerConfig(
-            RouteDefinitionLocator routeDefinitionLocator, SwaggerUiConfigProperties swaggerUiConfigProperties) {
-        this.routeDefinitionLocator = routeDefinitionLocator;
+    public SwaggerConfig(RouteDefinitionLocator locator, SwaggerUiConfigProperties swaggerUiConfigProperties) {
+        this.locator = locator;
         this.swaggerUiConfigProperties = swaggerUiConfigProperties;
     }
 
     @PostConstruct
     public void init() {
         List<RouteDefinition> definitions =
-                routeDefinitionLocator.getRouteDefinitions().collectList().block();
+                locator.getRouteDefinitions().collectList().block();
         Set<AbstractSwaggerUiConfigProperties.SwaggerUrl> urls = new HashSet<>();
-
-        Objects.requireNonNull(definitions).stream()
+        definitions.stream()
                 .filter(routeDefinition -> routeDefinition.getId().matches(".*-service"))
                 .forEach(routeDefinition -> {
                     String name = routeDefinition.getId().replaceAll("-service", "");
