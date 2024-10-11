130c4e10afa1306b912b22e46dd9f0ff5867ac61
Jagdish Salgotra <jagdishsal@gmail.com>
2024-10-11 23:05:05 +0530

    bookstore frontend
    Took 1 hour 33 minutes

---
+++ bookstore-webapp/src/main/java/com/js/bookstore/webapp/web/controllers/ProductController.java
@@ -0,0 +1,21 @@
+package com.js.bookstore.webapp.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+class ProductController {
+
+    @GetMapping
+    String index() {
+        return "redirect:/products";
+    }
+
+    @GetMapping("/products")
+    String productsPage(@RequestParam(name = "page", defaultValue = "1") int pageNo, Model model) {
+        model.addAttribute("pageNo", pageNo);
+        return "products";
+    }
+}
