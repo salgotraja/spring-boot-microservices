bce8dbbf3a70db7bbe612e2e992f587c4e149fb6
Jagdish Salgotra <jagdishsal@gmail.com>
2024-10-17 20:33:27 +0530

    monitoring using grafana, promethues and loki
    Took 1 hour 13 minutes

--- catalog-service/src/main/java/com/js/bookstore/catalog/web/controllers/ProductController.java
+++ catalog-service/src/main/java/com/js/bookstore/catalog/web/controllers/ProductController.java
@@ -4,12 +4,15 @@ import com.js.bookstore.catalog.domain.PagedResult;
 import com.js.bookstore.catalog.domain.Product;
 import com.js.bookstore.catalog.domain.ProductNotFoundException;
 import com.js.bookstore.catalog.domain.ProductService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/products")
 class ProductController {
+    private static final Logger logger = LoggerFactory.getLogger(ProductController.class);
     private final ProductService productService;
 
     ProductController(ProductService productService) {
@@ -18,12 +21,14 @@ class ProductController {
 
     @GetMapping
     PagedResult<Product> getProduct(@RequestParam(name = "page", defaultValue = "1") int pageNo) {
+        logger.info("Fetching products for page: {}", pageNo);
         return productService.getProducts(pageNo);
     }
 
     @GetMapping("/{code}")
     ResponseEntity<Product> getProductByCode(@PathVariable String code) {
         // sleep();
+        logger.info("Fetching product for code: {}", code);
         return productService
                 .getProductByCode(code)
                 .map(ResponseEntity::ok)
