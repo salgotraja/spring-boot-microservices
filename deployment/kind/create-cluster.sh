f9f7f410a031eeb88e541356da1b5c55355769f0
Jagdish Salgotra <jagdishsal@gmail.com>
2024-10-19 23:24:35 +0530

    k8s and kind cluster configuration
    Took 37 minutes

---
+++ deployment/kind/create-cluster.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+echo "Initializing Kubernetes cluster..."
+
+kind create cluster --name bookstore --config kind-config.yaml
+
+kubectl create namespace bookstore
+kubectl create namespace monitoring
+kubectl create namespace ingress-nginx
+
+
+echo "\n-----------------------------------------------------\n"
+
+echo "Installing NGINX Ingress..."
+
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
+
+echo "\n-----------------------------------------------------\n"
+
+echo "Waiting for NGINX Ingress to be ready..."
+
+sleep 10
+
+kubectl wait --namespace ingress-nginx \
+  --for=condition=ready pod \
+  --selector=app.kubernetes.io/component=controller \
+  --timeout=180s
+
+echo "\n"
+
+echo "Happy Sailing!"
