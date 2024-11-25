bb259f9bd16657466a429eb41c4e37ed07311db5
Jagdish Salgotra <jagdishsal@gmail.com>
2024-11-25 23:11:42 +0530

    cleanup

--- bookstore/app/(auth)/layout.tsx
+++ bookstore/app/(auth)/layout.tsx
@@ -1,11 +1,30 @@
-import React from "react";
+import Image from "next/image";
 
-const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+import React, { ReactNode } from "react";
+
+const AuthLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <div>
-      <div>Auth layout</div>
-      {children}
-    </div>
+    <main className="flex min-h-screen items-center justify-center bg-cover bg-center bg-no-repeat px-4 py-10">
+      <section className="min-w-full rounded-[10px] border px-4 py-10 shadow-md sm:min-w-[520px] sm:px-8">
+        <div className="flex items-center justify-between gap-2">
+          <div className="space-y-2.5">
+            <h1 className="font-bold">Join Bookstore</h1>
+            <p className="">To get your questions answered</p>
+          </div>
+          <Image
+            src="images/books.png"
+            alt="Bookstore Logo"
+            width={50}
+            height={50}
+            className="object-contain"
+          />
+        </div>
+
+        {children}
+
+        {/* <SocialAuthForm /> */}
+      </section>
+    </main>
   );
 };
 
